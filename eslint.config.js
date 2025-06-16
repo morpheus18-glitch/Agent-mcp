e7809498819a6fb705f0966290492c44ab7f773d
morpheus18-glitch <acwilliams.18@gmail.com>
2025-06-15 22:04:30 -0400

    Provide ESLint flat config for v9

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {},
+};
